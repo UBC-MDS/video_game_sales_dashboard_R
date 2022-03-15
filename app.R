0353c2389326d3941066ac2b580d9d9d6b44a289
Amelia Tang <amelia9@student.ubc.ca>
2022-03-14 17:55:01 -0700

    Add dashboard structure and update init.R

--- app.R
+++ app.R
@@ -32,6 +32,17 @@ ps4_NA <- ps4  %>%
   summarise(NA_sales = sum(North.America))  %>%
   slice(1:6)
 
+xbox_global <- xbox  %>%
+    select(Year, Global)  %>%
+    group_by(Year)  %>%
+    summarise(global_sales = sum(Global))  %>%
+    slice(1:6)
+
+ps4_global <- ps4  %>%
+    select(Year, Global)  %>%
+    group_by(Year)  %>%
+    summarise(global_sales= sum(Global))  %>%
+    slice(1:6)
 
 year_range <- seq(min(ps4_NA$Year), max(ps4_NA$Year))
 year_range_label <- setNames(as.list(as.character(year_range)), as.integer(year_range))
@@ -45,7 +56,9 @@ sidebar <- dbcCol(
         htmlBr(),
         htmlH4("Video Game Sales Analytics App", className = "Heading-3", style=list("text-align"='center')),
         htmlHr(),
-        htmlP("Select Companies", className = "Heading-5", style=list("text-align"='center')),
+        htmlP("A dashboard to analyze sales of major players in the video game industry", style=list("text-align"='center')),
+        htmlHr(),
+        htmlH6("Sales Trend Companies", className = "Heading-5", style=list("text-align"='center')),
         dbcCol(
             list(
                 dccDropdown(
@@ -58,39 +71,88 @@ sidebar <- dbcCol(
                     value = c("ps4"),
                 )
             ),
-            width = 10,
+            width = 15,
             style = list("padding" = "10px 10px 10px 0px"),
-        )
+        ),
+        htmlHr(),
+        htmlH6("Market Share Year", className = "Heading-5", style=list("text-align"='center')),
+        htmlBr(),
+        htmlHr(),
+        htmlH6("Top Genres Year", className = "Heading-5", style=list("text-align"='center')),
+        htmlBr(),
+        htmlHr(),
+        htmlH6("Top Publisher Year", className = "Heading-5", style=list("text-align"='center')),
+        htmlBr(),
+        htmlHr(),
+        htmlH6("Critic Score Year", className = "Heading-5", style=list("text-align"='center')),
+        htmlBr(),
+        htmlHr(),
+        htmlP("
+        This dashboard was made by Amelia Tang, 
+        Alex Yinan Guo, Yike Shi, and Mahmoodur Rahman.  
+        Last updated 2022-03-14")
     ),
     style = list("background-color" = "#ADD8E6"),
-    md=2
-)
+    md=2)
 
 # ==============================================================================
 #                            content
 # ==============================================================================
 
-content <- dbcCol(
-    list(
-        htmlBr(),
-        dccGraph(id='sales_plot'),
-        dccSlider(
-            id="slider-year",
-            min=min(xbox_NA$Year),
-            max=max(xbox_NA$Year),
-            step=1,
-            value=max(xbox_NA$Year),
-            marks=year_range_label,
-            tooltip=list(
-                always_visible=TRUE,
-                placement="top"
-            ))))
-
+content <- dbcCol(list(
+    dccTabs(id="tabs", children=list(
+        dccTab(label='North America', children=list(dbcRow(list(dbcCol(
+            list(
+                htmlBr(),
+                dccGraph(id='sales_plot'),
+                dccSlider(
+                    id="slider-year",
+                    min=min(xbox_NA$Year),
+                    max=max(xbox_NA$Year),
+                    step=1,
+                    value=2017,
+                    marks=year_range_label,
+                    tooltip=list(
+                        always_visible=TRUE,
+                        placement="top")))),
+            dbcCol(list(
+                htmlH4("Alex NA Market Share Placeholder"))))),
+            dbcRow(list(
+                dbcCol(list(
+                    htmlH4("Maeve NA Top Grene Placeholder"))),
+                dbcCol(list(
+                    htmlH4("Maeve NA Top Publisher Placeholder"))),
+                dbcCol(list(
+                    htmlH4("Alex NA Critic Score Placeholder"))))))),
+        dccTab(label='Global', children=list(dbcRow(list(dbcCol(
+            list(
+                htmlBr(),
+                dccGraph(id='sales_plot_global'),
+                dccSlider(
+                    id="slider-year2",
+                    min=min(xbox_NA$Year),
+                    max=max(xbox_NA$Year),
+                    step=1,
+                    value=2017,
+                    marks=year_range_label,
+                    tooltip=list(
+                        always_visible=TRUE,
+                        placement="top")))),
+            dbcCol(list(
+                htmlH4("Alex GB Market Share Placeholder"))))),
+            dbcRow(list(
+                dbcCol(list(
+                    htmlH4("Maeve GB Top Grene Placeholder"))),
+                dbcCol(list(
+                    htmlH4("Maeve GB Top Publisher Placeholder"))),
+                dbcCol(list(
+                    htmlH4("Alex GB Critic Score Placeholder")))))))))))
 
 # ==============================================================================
 #                            layout
 # ==============================================================================
 
+   
 app$layout(
     dbcContainer(
         list(dbcRow(
@@ -101,7 +163,6 @@ app$layout(
         style = list("height" = "100vh", "width"="80%")
     )
 )
-
             
 # ==============================================================================
 #                            Callbacks and backend
@@ -132,6 +193,35 @@ app$callback(
              layout(title =('Sales Trend'))
       }
       ggplotly(p)
+    }
+)
+
+app$callback(
+    output('sales_plot_global', 'figure'),
+    list(input('company-select', 'value'),
+         input("slider-year2", "value")),
+    function(company, year) {
+        if (company == 'ps4') {
+            ps4_global_yr <- ps4_global  %>%
+                filter(Year <= year)  %>%
+                rename(Sales = global_sales)
+            p <- plot_ly(ps4_global_yr, x = ~Year, y = ~Sales, type = 'scatter', mode = 'lines')  %>%
+                layout(title =('Sales Trend'))
+        } else if (company == 'xbox'){
+            xbox_global_yr <- xbox_global  %>%
+                filter(Year <= year)  %>%
+                rename(Sales = global_sales)
+            p <- plot_ly(xbox_global_yr, x = ~Year, y = ~Sales, type = 'scatter', mode = 'lines')  %>%
+                layout(title =('Sales Trend'))
+        } else {
+            global_sales_plot <- merge(ps4_global, xbox_global, by = 'Year') %>%
+                rename(Sales = global_sales.x)  %>%
+                filter(Year <= year)
+            p <- plot_ly(global_sales_plot, x = ~Year, y = ~Sales, name = "PlayStation4", type = 'scatter', mode = 'lines')
+            p <- p  %>% add_trace(y = ~global_sales.y, name = 'XBox', mode = 'lines')  %>%
+                layout(title =('Sales Trend'))
+        }
+        ggplotly(p)
     }
 )
 
